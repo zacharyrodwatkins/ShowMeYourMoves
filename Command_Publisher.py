443ae9793fef0a840fe445cb0c9b929ac13bda7c
gregory-j-r <43014180+gregory-j-r@users.noreply.github.com>
2019-10-28 16:13:25 -0700

    Add files via upload

--- Command_Publisher.py
+++ Command_Publisher.py
@@ -1,11 +1,12 @@
 import os
 import time
 path = "> ~/.local/share/dolphin-emu/Pipes/pipe1"
+Button_Pause = 1.0/60.0
 
 
 def Press_Button(Button):
     os.system('echo' + ' ' + 'PRESS ' + Button + ' ' + path)
-    time.sleep(0.001)
+    time.sleep(Button_Pause)
     os.system('echo' + ' ' + 'RELEASE ' + Button + ' ' + path)
 
 
@@ -29,4 +30,47 @@ def Show_Your_Moves():
         Press_Button('D_UP')
 
 
-# Show_Your_Moves()
+def Wavedash(Direction):
+    if Direction == "LEFT":
+        Press_Button('X')
+        time.sleep(0.05)
+        Move_Stick('0', '1')
+        Hold_Button('L', Button_Pause)
+        Move_Stick('0.5', '0.5')
+
+    if Direction == "RIGHT":
+        Press_Button('X')
+        time.sleep(0.05)
+        Move_Stick('1', '1')
+        Hold_Button('L', Button_Pause)
+        Move_Stick('0.5', '0.5')
+
+
+def Hold_Button(Button, TIME):
+    os.system('echo' + ' ' + 'PRESS ' + Button + ' ' + path)
+    time.sleep(TIME)
+    os.system('echo' + ' ' + 'RELEASE ' + Button + ' ' + path)
+
+
+# Wavedash('LEFT')
+# time.sleep(0.15)
+# Wavedash('RIGHT')
+
+def Short_Hop_Attack(Attack):
+    if Attack == 'NEUTRAL':
+
+    
+    if Attack == 'UP':
+
+    
+    if Attack == 'STOMP':
+
+    
+    if Attack == 'KNEE':
+        Press_Button('X')
+        time.sleep(Button_Press)
+    
+    if Attack == 'BACK_SLAP':
+
+
+    
