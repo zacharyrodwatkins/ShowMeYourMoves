c6384916b004ff09079a1511c4bc4fedc4a2dc35
gregory-j-r <43014180+gregory-j-r@users.noreply.github.com>
2019-10-28 16:49:15 -0700

    Add files via upload

--- Command_Publisher.py
+++ Command_Publisher.py
@@ -2,6 +2,7 @@ import os
 import time
 path = "> ~/.local/share/dolphin-emu/Pipes/pipe1"
 Button_Pause = 1.0/60.0
+After_jump_Pause = 0.05
 
 
 def Press_Button(Button):
@@ -33,14 +34,14 @@ def Show_Your_Moves():
 def Wavedash(Direction):
     if Direction == "LEFT":
         Press_Button('X')
-        time.sleep(0.05)
+        time.sleep(After_jump_Pause)
         Move_Stick('0', '1')
         Hold_Button('L', Button_Pause)
         Move_Stick('0.5', '0.5')
 
     if Direction == "RIGHT":
         Press_Button('X')
-        time.sleep(0.05)
+        time.sleep(After_jump_Pause)
         Move_Stick('1', '1')
         Hold_Button('L', Button_Pause)
         Move_Stick('0.5', '0.5')
@@ -52,25 +53,95 @@ def Hold_Button(Button, TIME):
     os.system('echo' + ' ' + 'RELEASE ' + Button + ' ' + path)
 
 
-# Wavedash('LEFT')
-# time.sleep(0.15)
-# Wavedash('RIGHT')
+Wavedash('LEFT')
+time.sleep(0.15)
+Wavedash('RIGHT')
+time.sleep(0.3)
 
-def Short_Hop_Attack(Attack):
-    if Attack == 'NEUTRAL':
 
-    
-    if Attack == 'UP':
-
-    
-    if Attack == 'STOMP':
-
-    
-    if Attack == 'KNEE':
+def Short_Hop_Attack(Direction):
+    if Direction == 'NEUTRAL':
         Press_Button('X')
-        time.sleep(Button_Press)
-    
-    if Attack == 'BACK_SLAP':
+        time.sleep(After_jump_Pause)
+        Press_Button('A')
+
+    if Direction == 'UP':
+        Press_Button('X')
+        time.sleep(After_jump_Pause)
+        Move_Stick('0.5', '0')
+        Press_Button('A')
+        Move_Stick('0.5', '0.5')
+
+    if Direction == 'DOWN':
+        Press_Button('X')
+        time.sleep(After_jump_Pause)
+        Move_Stick('0.5', '1')
+        Press_Button('A')
+        Move_Stick('0.5', '0.5')
+
+    if Direction == 'LEFT':
+        Press_Button('X')
+        time.sleep(After_jump_Pause)
+        Move_Stick('0', '0.5')
+        Press_Button('A')
+        Move_Stick('0.5', '0.5')
+
+    if Direction == 'RIGHT':
+        Press_Button('X')
+        time.sleep(After_jump_Pause)
+        Move_Stick('1', '0.5')
+        Press_Button('A')
+        Move_Stick('0.5', '0.5')
 
 
-    
+Short_Hop_Attack('DOWN')
+time.sleep(1)
+Short_Hop_Attack('RIGHT')
+time.sleep(1)
+
+
+def Smash_Attack(Direction, Charge):
+    if Direction == 'UP':
+        Move_Stick('0.5', '0')
+        Hold_Button('A', Charge)
+        Move_Stick('0.5', '0.5')
+
+    if Direction == 'DOWN':
+        Move_Stick('0.5', '1')
+        Hold_Button('A', Charge)
+        Move_Stick('0.5', '0.5')
+
+    if Direction == 'LEFT':
+        Move_Stick('0', '0.5')
+        Hold_Button('A', Charge)
+        Move_Stick('0.5', '0.5')
+
+    if Direction == 'RIGHT':
+        Move_Stick('1', '0.5')
+        Hold_Button('A', Charge)
+        Move_Stick('0.5', '0.5')
+
+
+Smash_Attack('RIGHT', 0.5)
+time.sleep(1)
+
+
+def Special_Move(Direction):
+    if Direction == 'DOWN':
+        Move_Stick('0.5', '1')
+        Press_Button('B')
+        Move_Stick('0.5', '0.5')
+
+    if Direction == 'LEFT':
+        Move_Stick('0', '0.5')
+        Press_Button('B')
+        Move_Stick('0.5', '0.5')
+
+    if Direction == 'RIGHT':
+        Move_Stick('1', '0.5')
+        Press_Button('B')
+        Move_Stick('0.5', '0.5')
+
+
+Special_Move('LEFT')
+time.sleep(1)
